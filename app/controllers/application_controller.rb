9f39575b3a546a7ce5acc06a1238db327bf91cba
Manuel y Adan <manuman93734@gmail.com>
2025-09-25 22:53:05 -0600

    segunda accion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def saludo
     render html: "¡Bienvenido a mi segunda acción en Rails!"
   end
+  def tarea 
+    render html: "chinandega es un mal lugar para vivir"
+  end
 end
+ 
